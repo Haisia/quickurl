eb0a441653f0d6e31d6c24bfee306742dee41c35
최준혁 <cwnsgur13579@gmail.com>
2025-10-30 15:28:32 +0900

    fix: UserinfoResponse 응답값 필드명 변경

--- src/main/kotlin/dev/haisia/quickurl/adapter/web/api/auth/dto/UserInfoResponse.kt
+++ src/main/kotlin/dev/haisia/quickurl/adapter/web/api/auth/dto/UserInfoResponse.kt
@@ -1,6 +1,10 @@
 package dev.haisia.quickurl.adapter.web.api.auth.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class UserInfoResponse(
+  @JsonProperty("email")
   val email: String,
+  @JsonProperty("is_logged_in")
   val isLoggedIn: Boolean = true
 )
